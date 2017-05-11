518b574a490859a6b3f0e954d098cbd0f877077a
Benjamin Baier <programmer@netzbasis.de>
2017-05-11 22:39:50 +0200

    Upgrade.sh tweak - use /bin/sh -e

--- files/scripts/Upgrade.sh
+++ files/scripts/Upgrade.sh
@@ -1,21 +1,20 @@
-#!/bin/sh
-echo "moving kernel" &&
-cp bsd.mp /nbsd &&
-ln -f /bsd /obsd &&
-cp bsd /bsd.sp &&
-cp bsd.rd /bsd.rd &&
-mv /nbsd /bsd &&
-echo "extracting base system" &&
+#!/bin/sh -e
+echo "moving kernel"
+cp bsd.mp /nbsd
+ln -f /bsd /obsd
+cp bsd /bsd.sp
+cp bsd.rd /bsd.rd
+mv /nbsd /bsd
+echo "extracting base system"
 for file in *.tgz
 do
 	echo "untar: $file"
 	tar -zxphf $file -C /
-	[ $? -gt 0 ] && exit 1
 done 
-echo "running MAKEDEV" &&
-cd /dev &&
-./MAKEDEV all &&
-echo "running sysmerge" &&
-sysmerge &&
+echo "running MAKEDEV"
+cd /dev
+./MAKEDEV all
+echo "running sysmerge"
+sysmerge
 echo "upgrade complete. restart your system now."
-exit 0
+
